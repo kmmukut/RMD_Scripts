699e5eb34961fc3bdbe07435b750de2817b13ea9
Khaled Mosharraf Mukut <kmmukut@Khaleds-MacBook-Pro.local>
2023-10-04 01:27:42 -0700

    add timer in the c codes

--- sample_every_nth_step_bonds.c
+++ sample_every_nth_step_bonds.c
@@ -5,10 +5,13 @@
 
 int main(int argc, char *argv[])
 {
-    clock_t start, end; // Declare start and end variables
+    clock_t start_cpu, end_cpu;  // Declare start and end variables for CPU time
+    time_t start_wall, end_wall; // Declare start and end variables for wall-clock time
     double cpu_time_used;
+    double wall_time_used;
 
-    start = clock(); // Start the timer
+    start_cpu = clock();     // Start the CPU timer
+    start_wall = time(NULL); // Start the wall-clock timer
 
     if (argc != 4)
     {
@@ -51,10 +54,13 @@ int main(int argc, char *argv[])
     fclose(inputFile);
     fclose(outputFile);
 
-    end = clock();                                            // End the timer
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // Calculate time taken
+    end_cpu = clock();                                                // End the CPU timer
+    end_wall = time(NULL);                                            // End the wall-clock timer
+    cpu_time_used = ((double)(end_cpu - start_cpu)) / CLOCKS_PER_SEC; // Calculate CPU time taken
+    wall_time_used = difftime(end_wall, start_wall);                  // Calculate wall-clock time taken
 
-    printf("Time taken: %f seconds\n", cpu_time_used); // Print time taken
+    printf("CPU Time taken: %f seconds\n", cpu_time_used);         // Print CPU time taken
+    printf("Wall-clock Time taken: %f seconds\n", wall_time_used); // Print wall-clock time taken
 
     return 0;
 }
